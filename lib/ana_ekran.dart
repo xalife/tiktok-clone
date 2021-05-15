27c4585c8f66c8bb0c927e1fb54c03bda7842aa4
MAC <mac@MAC-MacBook-Air.local>
2021-05-15 23:04:26 +0300

    top bar eklendi

--- lib/ana_ekran.dart
+++ lib/ana_ekran.dart
@@ -1,3 +1,5 @@
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:preload_page_view/preload_page_view.dart';
 
@@ -13,6 +15,7 @@ class _AnaEkranState extends State<AnaEkran> {
   PreloadPageController _pageController;
   bool isPageStable = false;
   int current = 0;
+  bool isFirstSelected = false;
   @override
   void initState() {
     super.initState();
@@ -39,21 +42,139 @@ class _AnaEkranState extends State<AnaEkran> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: Theme(
+        data: ThemeData(
+          canvasColor: Colors.black,
+        ),
+        child: Container(
+          color: Colors.black,
+          padding: EdgeInsets.symmetric(horizontal: 5.0),
+          child: BottomNavigationBar(
+            currentIndex: 2,
+            iconSize: 32,
+            unselectedLabelStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.w100),
+            selectedLabelStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.w100),
+            showUnselectedLabels: true,
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(
+                  EvaIcons.home,
+                ),
+                label: "Ana Sayfa",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  EvaIcons.search,
+                ),
+                label: "Keşfet",
+              ),
+              BottomNavigationBarItem(
+                icon: Image(
+                  fit: BoxFit.fill,
+                  image: AssetImage("assets/middle_btn.png"),
+                  width: 55,
+                  height: 35,
+                ),
+                label: "    ",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  EvaIcons.messageSquare,
+                ),
+                label: "Mesajlar",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  EvaIcons.person,
+                ),
+                label: "Ben",
+              ),
+            ],
+          ),
+        ),
+      ),
       body: Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
-        child: PreloadPageView.builder(
-          controller: _pageController,
-          preloadPagesCount: 3,
-          itemCount: liste.length,
-          scrollDirection: Axis.vertical,
-          itemBuilder: (context, index) {
-            return VideoPost(
-              video_uri: liste[index],
-              pageIndex: index,
-              currentPageIndex: current,
-            );
-          },
+        child: Stack(
+          children: [
+            PreloadPageView.builder(
+              controller: _pageController,
+              preloadPagesCount: 3,
+              itemCount: liste.length,
+              scrollDirection: Axis.vertical,
+              itemBuilder: (context, index) {
+                return VideoPost(
+                  video_uri: liste[index],
+                  pageIndex: index,
+                  currentPageIndex: current,
+                );
+              },
+            ),
+            SafeArea(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        isFirstSelected = true;
+                      });
+                    },
+                    child: AnimatedDefaultTextStyle(
+                      duration: Duration(milliseconds: 200),
+                      curve: Curves.easeIn,
+                      style: isFirstSelected
+                          ? TextStyle(
+                              fontSize: 21,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w800,
+                              fontFamily: "SansPro",
+                            )
+                          : TextStyle(
+                              color: Colors.black,
+                              fontFamily: "SansPro",
+                            ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Text("Takip Ediliyor"),
+                      ),
+                    ),
+                  ),
+                  Text(
+                    "|",
+                    style: TextStyle(color: Colors.white60),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        isFirstSelected = false;
+                      });
+                    },
+                    child: AnimatedDefaultTextStyle(
+                      duration: Duration(milliseconds: 200),
+                      curve: Curves.easeIn,
+                      style: (!isFirstSelected)
+                          ? TextStyle(
+                              fontSize: 21,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w800,
+                              fontFamily: "SansPro",
+                            )
+                          : TextStyle(
+                              color: Colors.black,
+                              fontFamily: "SansPro",
+                            ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Text("Sizin İçin"),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
