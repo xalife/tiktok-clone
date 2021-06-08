5850078e2439458740221bac90b7e7fc5c079fee
MAC <mac@MAC-MacBook-Air.local>
2021-06-08 19:30:58 +0300

    tiktok user info part implemented

--- lib/Elemets/video_post.dart
+++ lib/Elemets/video_post.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
@@ -40,10 +43,160 @@ class _VideoPostState extends State<VideoPost> {
       _playerController.pause();
     }
     return Scaffold(
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: VideoPlayer(_playerController),
+      body: Stack(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: VideoPlayer(_playerController),
+          ),
+          Positioned(
+            bottom: 0,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height * 0.6,
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: LayoutBuilder(
+                builder: (context, constraints) {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Container(
+                        height: constraints.maxHeight * 0.7,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Container(
+                              width: 64,
+                              height: 64,
+                              child: Stack(
+                                children: [
+                                  CircleAvatar(
+                                    backgroundColor: Colors.white,
+                                    radius: 48,
+                                    child: CircleAvatar(
+                                      radius: 30,
+                                      backgroundImage: AssetImage("assets/avatar.jpg"),
+                                    ),
+                                  ),
+                                  Align(
+                                    alignment: Alignment(0.0, 1.5),
+                                    child: Container(
+                                      width: 24,
+                                      height: 24,
+                                      decoration: BoxDecoration(
+                                        color: Colors.redAccent,
+                                        borderRadius: BorderRadius.circular(12),
+                                      ),
+                                      child: Center(
+                                        child: Icon(
+                                          EvaIcons.plus,
+                                          size: 16,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Column(
+                              children: [
+                                Icon(
+                                  EvaIcons.heart,
+                                  size: 48,
+                                  color: Colors.white,
+                                ),
+                                Text(
+                                  "25.4K",
+                                  style: TextStyle(color: Colors.white),
+                                )
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Icon(
+                                  EvaIcons.messageCircleOutline,
+                                  size: 48,
+                                  color: Colors.white,
+                                ),
+                                Text(
+                                  "876",
+                                  style: TextStyle(color: Colors.white),
+                                )
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Transform(
+                                  transform: Matrix4.rotationY(pi),
+                                  alignment: Alignment.center,
+                                  child: Icon(
+                                    EvaIcons.undo,
+                                    size: 48,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Text(
+                                  "123",
+                                  style: TextStyle(color: Colors.white),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: constraints.maxHeight * 0.3,
+                        child: Row(
+                          children: [
+                            Container(
+                              width: MediaQuery.of(context).size.width * 0.7,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "@ Kayra seni seviyorm",
+                                    style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.w500),
+                                  ),
+                                  Text(
+                                    "mükemmel woodwork çalışması 2021 çok iyi geçsin vs vs .. #woodwork #handcraft",
+                                    style: TextStyle(
+                                      color: Colors.white70,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                  Row(
+                                    children: [
+                                      Icon(
+                                        Icons.music_note_sharp,
+                                        color: Colors.white70,
+                                      ),
+                                      Text(
+                                        "Louis Armstrong - What a ...",
+                                        style: TextStyle(
+                                          color: Colors.white70,
+                                        ),
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                            Container(
+                              width: MediaQuery.of(context).size.width * 0.23,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
